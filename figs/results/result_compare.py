f099a0a6058fd0cba3163e540d0998e6121755da
Hengyang Zhao <hzhao877502@gmail.com>
2018-02-21 11:48:20 -0800

    Revise comparison figure

--- figs/results/result_compare.py
+++ figs/results/result_compare.py
@@ -7,23 +7,25 @@ from matplotlib import rc
 rc('font', family='serif')
 rc('text', usetex=True)
 
+colors = {'rnn': '#000040', 'svr': '#400040', 'other': '#004000'}
+
 accuracy = [
 
-        ('$\mathrm{RNN_1}$', 0.79),
-        ('$\mathrm{RNN_2}$', 0.976),
-        ('$\mathrm{RNN_3}$', 0.9929),
-        ('$\mathrm{RNN_1^H}$', 0.9989),
-        ('$\mathrm{RNN_2^H}$', 1),
-        ('$\mathrm{RNN_3^H}$', 0.9962),
+        ('$\mathrm{RNN_1}$', 0.79, colors['rnn']),
+        ('$\mathrm{RNN_2}$', 0.976, colors['rnn']),
+        ('$\mathrm{RNN_3}$', 0.9929, colors['rnn']),
+        ('$\mathrm{RNN_1^H}$', 0.9989, colors['rnn']),
+        ('$\mathrm{RNN_2^H}$', 1, colors['rnn']),
+        ('$\mathrm{RNN_3^H}$', 0.9962, colors['rnn']),
 
-        ('$\mathrm{SVR_4^{100}}$', 0.932),
-        ('$\mathrm{SVR_4^{500}}$', 0.9422),
-        ('$\mathrm{SVR_4^{1K}}$', 0.9468),
-        ('$\mathrm{SVR_5^{100}}$', 0.9736),
-        ('$\mathrm{SVR_5^{500}}$', 0.9726),
-        ('$\mathrm{SVR_5^{1K}}$', 0.9675),
+        ('$\mathrm{SVR_4^{100}}$', 0.932, colors['svr']),
+        ('$\mathrm{SVR_4^{500}}$', 0.9422, colors['svr']),
+        ('$\mathrm{SVR_4^{1K}}$', 0.9468, colors['svr']),
+        ('$\mathrm{SVR_5^{100}}$', 0.9736, colors['svr']),
+        ('$\mathrm{SVR_5^{500}}$', 0.9726, colors['svr']),
+        ('$\mathrm{SVR_5^{1K}}$', 0.9675, colors['svr']),
 
-        ('Dong2014', 0.92)
+        ('Dong2014', 0.92, colors['other'])
 ]
 
 objects = [item[0] for item in accuracy]
@@ -35,7 +37,11 @@ plt.figure(figsize=(9, 2)).subplots_adjust(left=0.06, right=0.94, top=0.92, bott
 
 plt.grid(linestyle='--', alpha=0.5, color='black')
 
-plt.bar(y_pos, heights, align='center', color='black')
+bar_list = plt.bar(y_pos, heights, align='center', width=0.5)
+
+for bar, accu in zip(bar_list, accuracy):
+    bar.set_color(accu[2])
+
 plt.xticks(y_pos, objects)
 plt.ylabel('Detection accuracy')
 plt.ylim([0.75, 1.01])
